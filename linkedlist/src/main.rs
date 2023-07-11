9eecb8bbea2a7afc892b82cfc6dbdf7d9786cf0f
Ion Dodon <ion.dodon@amdaris.com>
2023-07-11 12:54:35 +0300

    linkedlist

---
+++ linkedlist/src/main.rs
@@ -0,0 +1,58 @@
+#![allow(dead_code)]
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[derive(Debug)]
+struct LinkedList {
+    pub head: Option<Rc<RefCell<Node>>>,
+    pub tail: Option<Rc<RefCell<Node>>>,
+}
+
+#[derive(Debug)]
+struct Node {
+    pub val: i32,
+    pub prev: Option<Rc<RefCell<Node>>>,
+    pub next: Option<Rc<RefCell<Node>>>,
+}
+
+impl LinkedList {
+    fn new() -> LinkedList {
+        LinkedList { head: None, tail: None }
+    }
+
+    fn add_to_tail(&mut self, new_node: Node) {
+        let node_rc_refcell = Rc::new(RefCell::new(new_node));
+
+        match &self.tail {
+            Some(tail) => {
+                node_rc_refcell.clone().borrow_mut().prev = Some(tail.clone());
+                tail.clone().borrow_mut().next = Some(node_rc_refcell.clone());
+                self.tail = Some(node_rc_refcell.clone());
+            }
+            None => {
+                self.head = Some(node_rc_refcell.clone());
+                self.tail = Some(node_rc_refcell.clone());
+            }
+        }
+    }
+}
+
+fn main() {
+    let mut linked_list = LinkedList::new();
+
+    let node1 = Node { val: 21, prev: None, next: None };
+    linked_list.add_to_tail(node1);
+
+    let node2 = Node { val: 22, prev: None, next: None };
+    linked_list.add_to_tail(node2);
+
+    let node3 = Node { val: 23, prev: None, next: None };
+    linked_list.add_to_tail(node3);
+
+    let mut node = linked_list.head;
+    while let Some(node_rc_refcell) = node {
+        println!("{:?}", node_rc_refcell.clone().borrow().val);
+        node = node_rc_refcell.clone().borrow().next.clone();
+    }
+}
