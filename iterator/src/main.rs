345cd67ac962c0132622df5cd6edd4620a0015e4
Ion Dodon <ion.dodon@amdaris.com>
2023-07-14 16:13:22 +0300

    iterator

--- iterator/src/main.rs
+++ iterator/src/main.rs
@@ -1,5 +1,13 @@
 struct MyList<T> {
-    elements: &[T],
+    elements: [T],
+}
+
+impl Iterator for MyList<i32> {
+    type Item = ();
+
+    fn next(&mut self) -> Option<Self::Item> {
+        todo!()
+    }
 }
 
 fn main() {
