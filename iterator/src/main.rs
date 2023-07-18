d76a2ff4530a4f5700b52a5c8da90b4af6b37b02
Ion Dodon <iondodon2@gmail.com>
2023-07-18 14:25:54 +0300

    iterator

--- iterator/src/main.rs
+++ iterator/src/main.rs
@@ -1,15 +1,39 @@
+#[warn(dead_code)]
 struct MyList<T> {
-    elements: [T],
+    index: usize,
+    size: usize,
+    elements: [T; 5],
+}
+
+impl Default for MyList<i32> {
+    fn default() -> Self {
+        MyList {
+            index: 0,
+            size: 5,
+            elements: [1, 2, 3, 4, 5],
+        }
+    }
 }
 
 impl Iterator for MyList<i32> {
-    type Item = ();
+    type Item = i32;
 
     fn next(&mut self) -> Option<Self::Item> {
-        todo!()
+        if self.index == self.size {
+            return None;
+        }
+
+        let element = Some(self.elements[self.index]);
+        self.index += 1;
+
+        element
     }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let list = MyList::default();
+
+    for e in list {
+        println!("{:?}", e);
+    }
 }
